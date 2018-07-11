eca7284c8e924fe14fcfe6e6f519968ba7cbdaf9
josepmcgrath <josepmcgrath@gmail.com>
2018-07-11 12:36:51 -0400

    Initial Commit

---
+++ TCLDetection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TCLDetection
+//
+//  Created by Joseph McGrath on 7/11/18.
+//  Copyright © 2018 Joseph McGrath. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
