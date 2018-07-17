205b253e5e3a5d3783e4537744519e0e7157e494
josepmcgrath <josepmcgrath@gmail.com>
2018-07-17 16:23:41 -0400

    Added ability to train SVM, although currently can only do 7299 - 1 split

--- TCLDetection/main.cpp
+++ TCLDetection/main.cpp
@@ -23,7 +23,30 @@
 
 
 using namespace std;
-
+/* useful for identifying Mat object type
+string type2str(int type) {
+    string r;
+    
+    uchar depth = type & CV_MAT_DEPTH_MASK;
+    uchar chans = 1 + (type >> CV_CN_SHIFT);
+    
+    switch ( depth ) {
+        case CV_8U:  r = "8U"; break;
+        case CV_8S:  r = "8S"; break;
+        case CV_16U: r = "16U"; break;
+        case CV_16S: r = "16S"; break;
+        case CV_32S: r = "32S"; break;
+        case CV_32F: r = "32F"; break;
+        case CV_64F: r = "64F"; break;
+        default:     r = "User"; break;
+    }
+    
+    r += "C";
+    r += (chans+'0');
+    
+    return r;
+}
+*/
 
 int main(int argc, char *argv[]) {
     if (argc != 5) {
@@ -132,6 +155,7 @@ int main(int argc, char *argv[]) {
             histfile.open("histogram_single.csv", ios::out | ios::trunc);
             for (int i = 1; i < (histogram.size() - 1); i++) histfile << histogram[i] << ", ";
             histfile << histogram[histogram.size()-1] << endl;
+            histfile.close();
             break;
         }
         case 2: {
@@ -140,6 +164,30 @@ int main(int argc, char *argv[]) {
             svm->setType(cv::ml::SVM::C_SVC);
             svm->setKernel(cv::ml::SVM::LINEAR);
             svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 100, 1e-6));
+            
+            // Import training data from CSV file
+            cv::Ptr<cv::ml::TrainData> histData = cv::ml::TrainData::loadFromCSV("histogram.csv", 0, 1, -1); //Filename, lines to skip for header, label location, -1 means only one response variable
+            
+            // Test for error
+            if (histData->getNSamples() == 0) {
+                cout << "Error: Could not read training data file histogram.csv." << endl;
+                break;
+            }
+            
+            // Set split ratio
+            histData -> setTrainTestSplit(7299);
+            int n_train_samples = histData->getNTrainSamples();
+            cout << "The number of training samples is " << n_train_samples << " out of 7300 total samples." << endl;
+            // Train SVM
+            svm->train(histData, 0);
+            
+            cv::Mat testData = histData->getTestSamples();
+            cv::Mat testResponses = histData->getTestResponses();
+            
+            //int n_test_samples = histData->getNTestSamples();
+            
+            float result = svm->predict(testData);
+            cout << "The prediction is " << result << " and the actual value is " << testResponses << "." << endl;
             break;
         }
         default: {
